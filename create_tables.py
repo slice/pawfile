b064759096f3ab2bcb1c6e2d96da287e5894597e
slice <ryaneft@gmail.com>
2018-08-09 18:06:26 -0700

    force file path in create_tables

--- create_tables.py
+++ create_tables.py
@@ -1,13 +1,21 @@
+import sys
 import traceback
 
 from pawfile.models import File
 from pawfile.db import db
 
-try:
-    with db:
-        db.create_tables([File])
-except Exception:
-    traceback.print_exc()
-    print('\N{BALLOT X} Failed to create tables.')
-else:
-    print('\N{CHECK MARK} Created tables.')
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('usage: create_tables.py <database file>')
+        sys.exit(-1)
+
+    db.init(sys.argv[1])
+
+    try:
+        with db:
+            db.create_tables([File])
+    except Exception:
+        traceback.print_exc()
+        print('\N{BALLOT X} Failed to create tables.')
+    else:
+        print('\N{CHECK MARK} Created tables.')
